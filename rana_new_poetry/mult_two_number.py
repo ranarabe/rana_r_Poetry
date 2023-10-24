4314185fefcd5f8f63e9d452129869aad8301c80
Ranosh <my@gm.com>
2023-10-24 14:56:03 +0200

    add and use black

--- rana_new_poetry/mult_two_number.py
+++ rana_new_poetry/mult_two_number.py
@@ -1,13 +1,15 @@
 """Module providing a function Multiple two integer number."""
-def mult_two(a: int, b: int)->int:
-    '''
-        The function is multiple two integer number
-        
-     Parameters:
-            a (int) : Sirst input number
-            b (int) : Second input nuper 
-     Returns:
-            multiplation result 
-    '''
-    z=a*b
+
+
+def mult_two(a: int, b: int) -> int:
+    """
+       The function is multiple two integer number
+
+    Parameters:
+           a (int) : Sirst input number
+           b (int) : Second input nuper
+    Returns:
+           multiplation result
+    """
+    z = a * b
     return z
