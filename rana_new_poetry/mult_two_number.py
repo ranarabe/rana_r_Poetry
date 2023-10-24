6b8ec59afd79d4058e7b8fa11354aa343c8b7046
Ranosh <my@gm.com>
2023-10-24 14:20:46 +0200

    add pyline and run it to see the problem in my code then fix the problem

--- rana_new_poetry/mult_two_number.py
+++ rana_new_poetry/mult_two_number.py
@@ -1,5 +1,13 @@
-
-def mult_two(a: int, b: int) -> int:
-   
+"""Module providing a function Multiple two integer number."""
+def mult_two(a: int, b: int)->int:
+    '''
+        The function is multiple two integer number
+        
+     Parameters:
+            a (int) : Sirst input number
+            b (int) : Second input nuper 
+     Returns:
+            multiplation result 
+    '''
     z=a*b
     return z
